496c923413fcf82ac75db501312aece5430e5bdf
Shashank Naik <shashanknaik808@gmail.com>
2023-12-06 17:49:53 +0530

    style: Apply theme in ThemeProvider

--- mui-tut/src/index.js
+++ mui-tut/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { theme } from './theme';
+import { ThemeProvider } from '@emotion/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <ThemeProvider theme={theme}>
     <App />
+  </ThemeProvider>
 );
