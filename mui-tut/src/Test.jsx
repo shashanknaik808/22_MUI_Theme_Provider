6c8efdfe2af405f83061d60180decd38c6014fa3
Shashank Naik <shashanknaik808@gmail.com>
2023-12-06 19:44:19 +0530

    Updated xs breakpoint value to 0 (or another appropriate value)

--- mui-tut/src/Test.jsx
+++ mui-tut/src/Test.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Test() {
     return (
         <div>Test</div>
-    )
+    );
 }
 
 export default Test;
--- mui-tut/src/theme.jsx
+++ mui-tut/src/theme.jsx
@@ -13,7 +13,11 @@ export const theme = createTheme({
     },
     breakpoints: {
         values: {
-            xs: 200,
+            xs: 0,
+            sm: 600,
+            md: 960,
+            lg: 1280,
+            xl: 1920,
         }
     }
 });
